b6f8ca0e4c41871df203552a3ccbb1b1bb98b747
Thomas Pronold <tp@tpronold.de>
2015-06-11 15:13:08 +0200

    INIT

---
+++ desktop/src/de/tomgrill/gdxdialogs/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.tomgrill.gdxdialogs.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.tomgrill.gdxdialogs.app.GdxDialogsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxDialogsGame(), config);
+	}
+}
---
+++ html/src/de/tomgrill/gdxdialogs/app/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.tomgrill.gdxdialogs.app.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.tomgrill.gdxdialogs.app.GdxDialogsGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GdxDialogsGame();
+        }
+}
